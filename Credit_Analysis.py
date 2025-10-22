ddd67588d6cd9564b2d37ee7636f53792621f1e6
Paul Ledin <paul_ledin@uwalumni.com>
2025-10-22 16:33:37 -0500

    Update Credit_Analysis.py

--- Credit_Analysis.py
+++ Credit_Analysis.py
@@ -344,7 +344,7 @@ else:
         st.markdown('')
 
     with col[1]:          
-        st.markdown("""<right>XYZ Credit Union</right>""", unsafe_allow_html=True)
+        st.markdown("""<left>XYZ Credit Union</left>""", unsafe_allow_html=True)
         st.markdown("""<center>12345 Credit Union Blvd</center>""", unsafe_allow_html=True)
         st.markdown("""<center>Madison, WI</center>""", unsafe_allow_html=True)
 
