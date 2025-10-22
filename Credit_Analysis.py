cf110a45d090294a1de1498dfa8a2c85af084ee5
Paul Ledin <paul_ledin@uwalumni.com>
2025-10-22 16:50:30 -0500

    Update Credit_Analysis.py

--- Credit_Analysis.py
+++ Credit_Analysis.py
@@ -362,6 +362,7 @@ else:
         col = st.columns((2, 2, 2, 2, 2, 2, 2, 2, 2), gap='small')
         with col[0]:
             st.markdown('Assets')
+            st.markdown('---')
             st.markdown('Cash & Equivalents')
         
         with col[1]:
